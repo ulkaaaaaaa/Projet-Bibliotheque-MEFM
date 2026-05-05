24503c6e81a65a26870e51ba31aaf1942b89c6c5
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-05 10:35:14 +0200

    Update Interface.c

--- Interface.c
+++ Interface.c
@@ -8,11 +8,11 @@ void viderTampon() {
 
 //pour les différents choix dans le menu//
 
-void saisirEntierSecurise() {
+int saisirEntierSecurise() {
     int entier;
     while (scanf("%d", &entier) != 1) {
         printf("Erreur : tapez un chiffre.\n");
         viderTampon();
     }
     viderTampon();  //nettoie le \n restant//
-    return valeur;
+    return entier;
