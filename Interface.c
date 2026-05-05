4abee1e957ed32da8831da61f22aa0081137f913
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-05 10:29:35 +0200

    Update Interface.c
    Fonction saisirEntierSécuriser : Dans le menu pour choisir une option qui est un chiffre, pas de lettres acceptées

--- Interface.c
+++ Interface.c
@@ -1,4 +1,18 @@
+//retirer les valeures restantes//
+
 void viderTampon() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
+
+
+//pour les différents choix dans le menu//
+
+void saisirEntierSecurise() {
+    int entier;
+    while (scanf("%d", &entier) != 1) {
+        printf("Erreur : tapez un chiffre.\n");
+        viderTampon();
+    }
+    viderTampon();  //nettoie le \n restant//
+    return valeur;
