0403e3c3c2080933a4254e3ebcea68f5ecfea51f
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-11 22:34:25 +0200

    Update Interface.c
    -voir ligne 66 --> nom utilisateur ; nom de la variable exacte
    -est-ce que bon agencement avec nombre espace?
    -faire le message si vraiment veut se déconnecter
    -buffer où?

--- Interface.c
+++ Interface.c
@@ -1,6 +1,6 @@
 //retirer les valeures restantes//
 
-void viderTampon() {
+void viderbuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
@@ -12,47 +12,91 @@ int saisirEntierSecurise() {
     int entier;
     while (scanf("%d", &entier) != 1) {
         printf("Erreur : tapez un chiffre.\n");
-        viderTampon();
+        viderbuffer();
     }
-    viderTampon();  //nettoie le \n restant//
+    viderbuffer();  //nettoie le \n restant//
     return entier;
+}
 
 void menuDepart() {
     printf("╔═══════════════════════════════════════════════════════════════════════════════╗\n");
-    printf("║   ");
+    printf("║    ");
     printf("\033[1;36m");  //bleu pour tout le texte + gras avec "1"//
 
-    printf("Bienvenue dans la bibliothèque en ligne de votre établissement CY-Tech"    \n");
+    printf("Bienvenue dans la bibliothèque en ligne de votre établissement CY-Tech     ");
 
-    printf("\033[0m");   // retour noir //
-    printf("║");
-    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("╠═══════════════════════════════════════════════════════════════════════════════╣\n");
     printf("║");
 
     printf("\033[1;36m");  //bleu pour tout le texte//
     printf(" 1. Se connecter                                                               ");
 
-    printf("\033[0m");   // retour noir //
+    printf("\033[0m");   // retour blanc //
     printf("║\n");
-
+    printf("║");
     printf("\033[1;36m");  //bleu pour tout le texte//
     printf(" 2. Créer un compte                                                            ");
-    printf("║");
 
-    printf("\033[0m");   // retour noir //
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("║");
     printf("\033[31m");  // rouge //
     printf(" 3. Quitter                                                                    ");
 
-    printf("\033[0m"); // retour noir //
+    printf("\033[0m"); // retour blanc //
     printf("║\n");
     printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("\n");
+    printf("\n");
 
+    printf("▬▬▶Entrer le chiffre correspondant à votre demande.\n");
+    return 0;
 
+}
 
-
-    printf("▬▬▶Entrer le chiffre correspondant à votre demande."\n);
 
 
 void menuPrincipal() {
+    printf("╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║    ");
+    printf("\033[1;36m");  //bleu pour tout le texte + gras avec "1"//
+
+    printf("Nom utilisateur : %s", &nom.utilisateur); 
+
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("╠═══════════════════════════════════════════════════════════════════════════════╣\n");
+    printf("║");
+
+    printf("\033[1;36m");  //bleu pour tout le texte//
+    printf(" 1. Emprunter un nouveau livre                                               ");
+
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("║");
+    printf("\033[1;36m");  //bleu pour tout le texte//
+    printf(" 2. Rendre un livre                                                          ");
+
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("║");
+    printf("\033[1;36m");  //bleu pour tout le texte//
+    printf(" 3. Voir mes emprunts                                                          ");
+
+    printf("\033[0m");   // retour blanc //
+    printf("║\n");
+    printf("║");
+    printf("\033[31m");  // rouge //
+    printf(" 4. Se deconnecter                                                                    ");
+
+    printf("\033[0m"); // retour blanc //
+    printf("║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf("\n");
+
+    printf("▬▬▶Entrer le chiffre correspondant à votre demande.\n");
+    return 0;
 }
