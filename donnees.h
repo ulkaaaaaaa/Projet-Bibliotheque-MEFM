55b69f07db639f00d2956cceab66766349c7f54f
youssef alioua <youssefalioua208@gmail.com>
2026-05-12 09:02:55 +0200

    Update donnees.h

--- donnees.h
+++ donnees.h
@@ -6,35 +6,41 @@
 
 typdef struct {
   int id;
-char titre[100];
-char auteur[100];
-char categorie[50];
-int disponible;
+  char titre[100];
+  char auteur[100];
+  char categorie[50];
+  int disponible;
 } Livre;
 
 typedef struct {
-char login[50];
-char motdepasse[50];        /
-char role[20];
-int emprunts[MAX_EMPRUNTS];
-int nbEmprunts;
+  char login[50];
+  char motdepasse[50];        /
+  char role[20];
+  int emprunts[MAX_EMPRUNTS];
+  int nbEmprunts;
 } Utilisateur;
 
 extern Livre tabLivres[MAX_EMPRUNTS];
+extern UtilisateurstabUtilisateurs[MAX_UTILISATEURS];
 
-extern Utilisateurs
-tabUtilisateurs[MAX_UTILISATEURS];
 extern int nbUtilisateurs;
 extern int nbUtilisatuers;
+
 void initialiserBibliotheque()
+
 int chargerLivres(char nomFichier[]);
 int chargerUtilisateurs(char nomFichiers[]);
+
 int sauvegarderLivres(char nomFichiers[]);
 int sauvegarderUtilisateurs(char nomFichier[]);
+
 int authentifier(char login[], char motdepasse[]);
 int creerCompte(char login[], char motdepasse[], char role[]);
+
 void afficherLivres();
 void afficherUtilisateurs();
+
+#endif
 
 
 
