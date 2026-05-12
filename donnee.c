08fcd5b96492f1c3244273397d1b85c941d1046b
youssef alioua <youssefalioua208@gmail.com>
2026-05-12 09:30:51 +0200

    Update donnee.c

--- donnee.c
+++ donnee.c
@@ -2,5 +2,10 @@
 #include <string.h>
 #include "donnees.h"
 
-Livre tabLivres[MAX_LIVES];
-utilisateur tabUtilisat
+Livre tabLivres[MAX_LIVES]; 
+utilisateur tabUtilisateurs[MAX_UTILISATEURS];
+
+int nbLivres = 0;
+int nbUtilisaterus = 0;
+
+vo
