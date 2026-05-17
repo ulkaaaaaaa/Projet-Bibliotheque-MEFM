16cd981ea8cfd3adc560a1ec4714ff3ba830ef1c
youssef alioua <youssefalioua208@gmail.com>
2026-05-17 15:46:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,43 @@
+#include <stdio.h>
+#include "donnees.h"
+
 int main() {
+    int ok;
+    int indice;
+
+    initialiserBibliotheque();
+
+    ok = chargerLivres("livres.txt");
+    if (ok == 0) {
+        printf("Chargelent des livres impossible.\n");
+    }
+
+    ok = chargerUtilisateurs("utilisateurs.txt");
+    if (ok == 0) {
+        printf("Chargement des utilisateurs impossible.\n");
+    }
+
+    afficherLivres();
+    afficherUtilisateurs();
+
+    indice = authentifier("etudiant1", "1234");
+
+    if (indice != -1) {
+        printf("\nConnesion reussie : %s\n", tabUtilisateurs[indice].login);
+    } else {
+        printf("\nLogin ou mot de passe incorrect.\n");
+    }
+
+    ok = creerCompte("nouveau", "0000", "etudiant");
+
+    if (ok == 1) {
+        printf("\nCompte cree avec succes.\n");
+    } else{
+        printf("\nCreation du compte impossible.\n");
+    }
+
+    sauvegarderUtilisateurs("utilisateurs.txt");
+    sauvegarderLivres("livres.txt");
+
     return 0;
 }
