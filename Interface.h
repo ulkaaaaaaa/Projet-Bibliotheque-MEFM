f1b09c9c676426d394f0f78a40fc8196fe4b3cd9
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-18 00:32:02 +0200

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -5,15 +5,15 @@
 
 void viderbuffer();
 int saisirEntierSecurise();
-void afficherAccueil();
-void afficherStatusUtilisateur(Utilisateur u);
-void afficherMenuDepart();
-void afficherCreerCompte();
-int afficherSeConnecter();
-void afficherMenuPrincipal(Utilisateur u);
-int afficherVerifDeconnection();
-void afficherEmprunterLivre(Utilisateur u);
-void afficherRendreLivre(Utilisateur u);
+void afficherAccueil();   //logo debut avec chargement
+void afficherStatusUtilisateur(Utilisateur u);      //indication des livres en cours; emprunt et retour
+void afficherMenuDepart();      //choix entre creer un compte, se connecter ou quitter
+void afficherCreerCompte();      //creation d'un compte
+int afficherSeConnecter();          //connexion
+void afficherMenuPrincipal(Utilisateur u);        //choix entre emprunter/rendre un livre ou se deconnecter
+int afficherVerifDeconnexion();            //verification de deconnexion
+void afficherEmprunterLivre(Utilisateur u);            //choix entre rechercher par titre/auteur/catégorie ou retour au menu principal
+void afficherRendreLivre(Utilisateur u);        //affichage des livres à rendre
 
 #endif
 
