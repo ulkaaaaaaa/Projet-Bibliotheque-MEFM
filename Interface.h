378fc06a32d88d1baad4fbd7de752de8b89ed2e8
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-18 00:06:56 +0200

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -1,12 +1,21 @@
-void viderTampon();
-int saisirEntierSecurise();  //être sur que l'utilisateur entre bien un chiffre lors de ses choix//
-void menuDepart(Utilisateur u); //choix connexion, inscription//
-void seconnecter();  //connexion avec nom et mdp//
-void creeruncompte(); //Creation d'un nouveau compte avec mdp securisé//
-void menuPrincipal(); //choix emprunter, rendre deconnexion//
-int deconnection(); //Message pour demander si l'utilisateur est sur de vouloir se deconnecter//
-void emprunterlivre();  //choix entre rechercher un livre ou afficher la liste//
-void rendrelivre();  //
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "donnees.h"
+
+void viderbuffer();
+int saisirEntierSecurise();
+void afficherAccueil();
+void afficherStatusUtilisateur(Utilisateur u);
+void afficherMenuDepart();
+void afficherCreerCompte();
+int afficherSeConnecter();
+void afficherMenuPrincipal(Utilisateur u);
+int afficherVerifDeconnection();
+void afficherEmprunterLivre(Utilisateur u);
+void afficherRendreLivre(Utilisateur u);
+
+#endif
 
 
 
