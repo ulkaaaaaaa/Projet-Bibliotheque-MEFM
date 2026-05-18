be7760e3ba5958c878dd3ae56ce5e7c5da868e56
Jeannefournier <276527836+Jeannefournier@users.noreply.github.com>
2026-05-18 21:47:57 +0200

    Update Interface.h

--- Interface.h
+++ Interface.h
@@ -14,8 +14,7 @@ void afficherMenuPrincipal(Utilisateur u);        //choix entre emprunter/rendre
 int afficherVerifDeconnexion();            //verification de deconnexion
 void afficherEmprunterLivre(Utilisateur u);            //choix entre rechercher par titre/auteur/catégorie ou retour au menu principal
 void afficherRendreLivre(Utilisateur u);        //affichage des livres à rendre
-/* ajouter*/
-void afficherAjouterLivre(Utilisateur u);
+void afficherAjouterLivre(Utilisateur u);      //pour qu'un prof ajoute un nouveau livre
 
 #endif
 
