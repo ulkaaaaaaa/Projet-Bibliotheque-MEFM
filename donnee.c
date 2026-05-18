a19e927d37c8dac7d73babf6fa85bbdf04b5d977
ulkaaaaaaa <morikinulka@gmail.com>
2026-05-18 15:46:49 +0200

    Update donnee.c

--- donnee.c
+++ donnee.c
@@ -244,3 +244,131 @@ void afficherUtilisateurs() {
         printf("-----------------------------------------------\n");
     }
 }
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include "donnees.h" 
+
+/* --- CREATION DES VARIABLES GLOBALES PARTAGEES --- */
+/* C'est ici qu'on cree vraiment les tableaux dans la memoire */
+Livre tabLivres[MAX_LIVRES]; 
+Utilisateur tabUtilisateurs[MAX_UTILISATEURS];
+
+int nbLivres = 0;
+int nbUtilisateurs = 0;
+
+/* =========================================
+   INITIALISATION ET COMPTES
+   ========================================= */
+
+void initialiserBibliotheque(){
+    int i;
+    nbLivres = 0;
+    nbUtilisateurs = 0;
+
+    for (i = 0; i < MAX_LIVRES; i++){
+        tabLivres[i].id = 0;
+        tabLivres[i].estEmprunte = 0; 
+        tabLivres[i].dateRetour = 0;
+        strcpy(tabLivres[i].loginEmprunteur, "");
+    }
+    for (i = 0; i < MAX_UTILISATEURS; i++){
+        tabUtilisateurs[i].nbLivresActuels = 0;
+    }
+}
+
+int authentifier(char login[], char motDePasse[]){
+    int i;
+    for (i = 0; i < nbUtilisateurs; i++){
+        if (strcmp(tabUtilisateurs[i].login, login) == 0 &&
+            strcmp(tabUtilisateurs[i].mdp, motDePasse) == 0) { 
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int creerCompte(char login[], char motDePasse[], int role){
+    int i;
+
+    if(nbUtilisateurs >= MAX_UTILISATEURS) return 0;
+    if (role != ETUDIANT && role != PROFESSEUR) return 0; 
+
+    for (i = 0; i < nbUtilisateurs; i++) {
+        if (strcmp(tabUtilisateurs[i].login, login) == 0) return 0; 
+    }
+
+    strncpy(tabUtilisateurs[nbUtilisateurs].login, login, sizeof(tabUtilisateurs[nbUtilisateurs].login) - 1);
+    tabUtilisateurs[nbUtilisateurs].login[sizeof(tabUtilisateurs[nbUtilisateurs].login) - 1] = '\0'; 
+
+    strncpy(tabUtilisateurs[nbUtilisateurs].mdp, motDePasse, sizeof(tabUtilisateurs[nbUtilisateurs].mdp) - 1);
+    tabUtilisateurs[nbUtilisateurs].mdp[sizeof(tabUtilisateurs[nbUtilisateurs].mdp) - 1] = '\0'; 
+
+    tabUtilisateurs[nbUtilisateurs].role = role; 
+    tabUtilisateurs[nbUtilisateurs].nbLivresActuels = 0;
+
+    nbUtilisateurs++; 
+    return 1; 
+}
+
+void afficherUtilisateurs() {
+    int i;
+    printf("\n========== ANNUAIRE DES UTILISATEURS ==========\n");
+    if (nbUtilisateurs == 0) {
+        printf("Aucun utilisateur n'est inscrit pour le moment.\n===============================================\n");
+        return;
+    }
+    for (i = 0; i < nbUtilisateurs; i++) {
+        printf("Compte : %s\n", tabUtilisateurs[i].login);
+        if (tabUtilisateurs[i].role == PROFESSEUR) printf(" -> Statut : Professeur\n");
+        else printf(" -> Statut : Etudiant\n");
+        printf(" -> Livres en sa possession : %d\n-----------------------------------------------\n", tabUtilisateurs[i].nbLivresActuels);
+    }
+}
+
+/* =========================================
+   GESTION DES LIVRES
+   ========================================= */
+
+int ajouterLivre(char titre[], char auteur[], char categorie[]) {
+    if (nbLivres >= MAX_LIVRES) return 0; 
+
+    tabLivres[nbLivres].id = nbLivres + 1; 
+    
+    strncpy(tabLivres[nbLivres].titre, titre, sizeof(tabLivres[nbLivres].titre) - 1);
+    tabLivres[nbLivres].titre[sizeof(tabLivres[nbLivres].titre) - 1] = '\0';
+
+    strncpy(tabLivres[nbLivres].auteur, auteur, sizeof(tabLivres[nbLivres].auteur) - 1);
+    tabLivres[nbLivres].auteur[sizeof(tabLivres[nbLivres].auteur) - 1] = '\0';
+
+    strncpy(tabLivres[nbLivres].categorie, categorie, sizeof(tabLivres[nbLivres].categorie) - 1);
+    tabLivres[nbLivres].categorie[sizeof(tabLivres[nbLivres].categorie) - 1] = '\0';
+
+    tabLivres[nbLivres].estEmprunte = 0; 
+    tabLivres[nbLivres].dateRetour = 0;
+    strcpy(tabLivres[nbLivres].loginEmprunteur, "");
+
+    nbLivres++;
+    return 1; 
+}
+
+void afficherLivres() {
+    int i;
+    printf("\n--- Liste des livres ---\n");
+    if (nbLivres == 0) {
+        printf("La bibliotheque est vide pour le moment.\n");
+        return;
+    }
+    for (i = 0; i < nbLivres; i++) {
+        printf("%d | %s | %s | %s | ", tabLivres[i].id, tabLivres[i].titre, tabLivres[i].auteur, tabLivres[i].categorie);
+        if (tabLivres[i].estEmprunte == 0) printf("Disponible\n");
+        else printf("Emprunte\n");
+    }
+}
+
+
